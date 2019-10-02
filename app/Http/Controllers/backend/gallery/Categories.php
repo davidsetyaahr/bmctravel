26663af1af99359291e4923a2501ae81c01be5c7
Galih Bagus Prakasa <galihbagus555@gmail.com>
2019-10-02 20:08:39 +0700

    galih apdet
    select user galeri travel tips tag

--- app/Http/Controllers/backend/gallery/Categories.php
+++ app/Http/Controllers/backend/gallery/Categories.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers\backend\gallery;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Gallery_categories_model;
+
 class Categories extends Controller
 {
     function index()
     {
-        return view('backend.gallery.categories.index');
+        $galeri = Gallery_categories_model::all();
+        return view('backend.gallery.categories.list-categories-gallery' ,  ['id_category' => $galeri]);
+    }
+
+    function add()
+    {
+        return view('backend.gallery.categories.add-categories-gallery');
     }
 }
