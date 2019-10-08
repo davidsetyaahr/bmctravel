1ef372f30f571959dda32968c0ecdf2614c61120
nurilfm <43869363+nurilfm@users.noreply.github.com>
2019-10-08 22:24:43 +0700

    Update City.php

--- app/Http/Controllers/backend/data_master/City.php
+++ app/Http/Controllers/backend/data_master/City.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers\backend\data_master;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Province_model;
+use App\City_model;
+
 
 class City extends Controller
 {
     function index()
     {
-        return view('backend.data_master.city.index');
+        $cit = DB::table('city')->get();
+        return view('backend.data_master.city.index', ['city' => $cit]);
+        //return view('backend.data_master.city.index');
     }
     function add()
     {
-        return view('backend.data_master.city.add-city');
+        $prov = Province_model::all();
+        return view('backend.data_master.city.add-city' , ['id_province' => $prov]);
+    }
+    function store(Request $request)
+    {
+        //insert
+
+
+        DB::table('city')->insert([
+            'province_id' => $request->province_id,
+            'city_name' => $request->city_name,
+        ]);
+
+
+        return redirect('/admin/data-master/city');
     }
 }
+
