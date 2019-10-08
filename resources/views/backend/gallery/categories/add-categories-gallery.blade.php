5b531e78cccdacc2d18925629fed899ceaacd079
Galih Bagus Prakasa <galihbagus555@gmail.com>
2019-10-08 12:37:31 +0700

    insert categories

--- resources/views/backend/gallery/categories/add-categories-gallery.blade.php
+++ resources/views/backend/gallery/categories/add-categories-gallery.blade.php
@@ -10,11 +10,17 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
-                <label for="">Gallery Name</label>
-                <input type="text" class="form-control">
-                <br>
-                <button class="btn btn-primary" type="submit"><span class="mdi mdi-content-save"></span>  Save</button>
-                <button class="btn btn-secondary" type="reset"><span class="mdi mdi-refresh"></span> Reset</button>
+                <form action="/admin/gallery/add-categories-gallery" method="post">
+                @csrf
+                    <label for="">Category Name</label>
+                    <input type="text" class="form-control @error('category_name') is-invalid @enderror" name="category_name">
+                    @error('category_name')
+                        <div class="invalid-feedback"> {{ $message }} </div>
+                        @enderror
+                    <br>
+                    <button class="btn btn-primary" type="submit"><span class="mdi mdi-content-save"></span>  Save</button>
+                    <button class="btn btn-secondary" type="reset"><span class="mdi mdi-refresh"></span> Reset</button>
+                </form>
             </div>
         </div>
     </div>
