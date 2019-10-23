fc2a411af0dbdf27ad56cca60cd15984cb3dfc1e
Galih Bagus Prakasa <galihbagus555@gmail.com>
2019-10-23 09:26:29 +0700

    ccategory,tourtype
    insert edit show

--- app/Http/Controllers/backend/tour_package/TourType.php
+++ app/Http/Controllers/backend/tour_package/TourType.php
@@ -4,16 +4,58 @@ namespace App\Http\Controllers\backend\tour_package;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use  Illuminate\Support\Facades\DB;
+
+use App\Tour_type;
+use App\Gallery;
+use App\Gallery_categories;
+
 
 class TourType extends Controller
 {
-    function index()
+    public function index()
     {
-        return view('backend.tour_package.tour_type.list-tour-type');
+        $type = Tour_type::all();
+        return view('backend.tour_package.tour_type.list-tour-type' , ['tour_type' => $type]);
     }
 
-    function add()
+    public function add()
     {
-        return view('backend.tour_package.tour_type.add-tour-type');
+        $gallery = Gallery::all();
+        $gallery_categories = Gallery_categories::all();
+        return view('backend.tour_package.tour_type.add-tour-type' , ['gallery' => $gallery, 'categories' => $gallery_categories]);
+    }
+
+    public function store(Request $request)
+    {
+        //validasi
+        $request->validate([
+            'type_name' => 'required',
+            'id_gallery' => 'required'
+        ]);
+
+        //insert
+        DB::table('tour_type')->insert([
+            'type_name' => $request->type_name,
+            'id_gallery' => $request->id_gallery
+        ]);
+        return redirect('/admin/tour-package/tour-type')->with('status', 'Kategori hotel berhasil ditambahkan');
+    }
+
+    public function edit ($id)
+    {
+        $gallery = Gallery::all();
+        $gallery_categories = Gallery_categories::all();
+        $type = DB::table('tour_type')->where('id_type',$id)->get();
+        return view('backend.tour_package.tour_type.edit-tour-type', ['gallery' => $gallery, 'categories' => $gallery_categories, 'tour_type' => $type]);
+    }
+
+    public function update(Request $request)
+    {
+        DB::table('tour_type')->where('id_type',$request->id)->update([
+            'type_name' => $request->type_name,
+            'id_gallery' => $request->id_gallery
+        ]);
+        return redirect('/admin/tour-package/tour-type');
     }
 }
--- resources/views/backend/tour_package/tour_type/edit-tour-type.blade.php
+++ resources/views/backend/tour_package/tour_type/edit-tour-type.blade.php
@@ -1,8 +1,8 @@
 @extends('backend.template.main')
 @section('insert_caption','Insert New Tour Type')
 @section('view_caption','View All Tour Type')
-@section('insert_link','add-tour-type')
-@section('view_link','tour-type')
+@section('insert_link',url("admin/tour-package/add-tour-type"))
+@section('view_link',url("admin/tour-package/tour-type"))
 @section('view_status','')
 @section('insert_status','active')
 @section('admin')
@@ -10,14 +10,17 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
+            @foreach ($tour_type as $item)
+
                 @include('backend.gallery.gallery-template.select-gallery', ['type' => 'single', 'gallery' => $gallery, 'categories' => $categories])
-                <form action="/admin/tour-package/add-tour-type" method="post">
-                @csrf
+                <form action="/admin/tour-package/tour-type/update" method="post">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $item->id_type}}">
                     <label for="">Tour Type Name</label>
-                    <input type="text" class="form-control @error('type_name') is-invalid @enderror" name="type_name">
+                    <input type="text" class="form-control @error('type_name') is-invalid @enderror" name="type_name" value=" {{ $item->type_name }}">
                     @error('type_name')
-                        <div class="invalid-feedback"> {{ $message }} </div>
-                        @enderror
+                    <div class="invalid-feedback"> {{ $message }} </div>
+                    @enderror
                     <br>
                     <label for="">Gallery</label>
                     @include('backend.gallery.gallery-template.gallery-hidden')
@@ -25,6 +28,7 @@
                     <button class="btn btn-primary" type="submit"><span class="mdi mdi-content-save"></span>  Save</button>
                     <button class="btn btn-secondary" type="reset"><span class="mdi mdi-refresh"></span> Reset</button>
                 </form>
+            @endforeach
             </div>
         </div>
     </div>
