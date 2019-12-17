3bc300d6e50137e1516dbfa45137e1ffe91db298
adicahyaw <45073780+adicahyaw@users.noreply.github.com>
2019-12-17 21:23:57 +0700

    adi
    page 5

---
+++ resources/views/backend/tour_package/tour_package/add-package5.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+    <label for="meeting">Meeting Points:</label>
+    <textarea class="form-control" id="meeting"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="">Price</label>
+    <input type="number" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="Sale">Sale</label>
+    <input type="number" class="form-control">
+  </div>
+  <div class="float-right">
+    <button class="btn btn-primary" type="submit">Next <span class="mdi mdi-menu-right"></span></button>
+  </div>
