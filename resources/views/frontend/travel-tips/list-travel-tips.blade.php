9a3c87f97e131ebf87f64ebec6eeb86db83bab80
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2020-01-12 23:07:10 +0700

    h-1

--- resources/views/frontend/travel-tips/list-travel-tips.blade.php
+++ resources/views/frontend/travel-tips/list-travel-tips.blade.php
@@ -1,14 +1,15 @@
 @extends('frontend/common/template')
 @section('container')
-    <div class="hero-wrap js-fullheight-travel" style="background-image: url('public/direngine/images/bg_4.jpg');">
+<div style="background-image: url('{{url('public/direngine/images/bg_4.jpg')}}');"  class="js-fullheight-travel">
       <div class="overlay"></div>
       <div class="container">
-        <div class="row no-gutters slider-text js-fullheight-travel align-items-center justify-content-center" data-scrollax-parent="true">
-          <div class="col-md-7 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
-            <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>Travel Tips</span></p>
-            <h3 class="color-white" data-scrollax="properties: { translateY: '30%'}">Travel Tips</h3>
-            <div class="block-17 my-4">
-                <form action="{{url("travel-tips/search")}}" method="get" class="d-block d-flex">
+        <div class="row justify-content-center">
+
+          <div class="col-md-8 text-center mt-5 ftco-animate">
+
+          <h3 class="color-white">Travel Tips</h3>
+          <div class="block-17 my-4">
+          <form action="{{url("travel-tips/search")}}" method="get" class="d-block d-flex">
                   <div class="fields d-block d-flex">
                     <div class="textfield-search one-third">
                         <input type="text" class="form-control" placeholder="Search Here...">
@@ -16,13 +17,13 @@
                   </div>
                   <input type="submit" class="search-submit btn btn-primary" value="Search">
                 </form>
-              </div>
           </div>
+         </div>
         </div>
       </div>
-    </div>
+      </div>
 
-    <section class="ftco-section bg-light">
+    <section class="ftco-section">
        <div class="container">
         <div class="row d-flex">
             @foreach ($travel_tips as $trvltips)
